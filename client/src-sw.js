81f6049a05f5ca4b97c5c10b0ceed1f2fb1b078f
unknown <nataliematos12@gmail.com>
2023-05-18 11:48:22 -0400

    initial work on webpack and other TODOs

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  //  serve the request from the cache first
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      // cache the responses up to 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
